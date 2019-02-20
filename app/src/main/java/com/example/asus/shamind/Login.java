640fd58675caad81b637a5560bb84ec7e5b49f16
kazehiro <adriancodyokoteam@gmail.com>
2019-02-20 19:09:01 +0700

    Login & Register

--- app/src/main/java/com/example/asus/shamind/Login.java
+++ app/src/main/java/com/example/asus/shamind/Login.java
@@ -1,43 +1,208 @@
 package com.example.asus.shamind;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
     private TextView Register;
     private Button Login;
+    private Button Login_google;
+
+
+    private EditText etPassword, etEmail;
+
+    private long backPressedTime;
+    private Toast backToast;
+    private ProgressDialog progressDialog;
+    private int progressStatus = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        if (getIntent().getBooleanExtra("EXIT", false)) {
+            finish();
+        }
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    startActivity(new Intent(Login.this, Home.class));
+                } else {
+                    // User is signed out
+                }
+            }
+        };
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
         Register = (TextView) findViewById(R.id.btn_register);
-        Login    = (Button) findViewById(R.id.Btn_Login);
+        Login = (Button) findViewById(R.id.Btn_Login);
+        Login_google = (Button) findViewById(R.id.btnGoogle);
+        etEmail = (EditText) findViewById(R.id.email);
+        etPassword = (EditText) findViewById(R.id.password);
+
+        Login_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                                .setIsSmartLockEnabled(true)
+                                .build(),LOGIN_PERMISSION);*/
+                showSnackbar(v, "Layanan Belum Tersedia", 3000);
+
+            }
+        });
 
 
-     Register.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent mIntent = new Intent(Login.this, Register.class);
-             startActivity(mIntent);
-         }
-     });
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    startActivity(new Intent(Login.this, Home.class));
+                } else {
+                    // User is signed out
+                }
+            }
+        };
 
-     Login.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent mIntent = new Intent(Login.this, Home.class);
-             startActivity(mIntent);
-         }
-     });
 
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString();
+                String pass = etPassword.getText().toString();
+                System.out.println("HAHAHAHA = " + email + pass);
+                if (!email.equals("") && !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            startActivity(new Intent(Login.this, Home.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            toastMessage("Email atau Password Salah");
+                        }
+                    });
+                } else {
+                    showSnackbar(view, "Harap isi semua kolom", 3000);
+                    return;
+                }
+
+                progressDialog.setMax(100);
+                progressDialog.setMessage("Tunggu Sebentar");
+                progressDialog.setTitle("Login");
+
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setIndeterminate(false);
+                progressDialog.show();
+            progressStatus =0;
+            final Handler handle = new Handler(){
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    progressDialog.incrementProgressBy(1);
+                }
+            };
+                new
+
+            Thread(new Runnable() {
+                @Override
+                public void run () {
+                    while (progressStatus < 100) {
+                        progressStatus += 1;
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        handle.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressDialog.setProgress(progressStatus);
+                                if (progressStatus == 100) {
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        });
+                    }
+                }
+            }).
+
+            start();
+        }
+    });
+
+
+
+     Register.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        Intent mIntent = new Intent(Login.this, Register.class);
+        startActivity(mIntent);
+    }
+    });
+}
+
+
+    public void showSnackbar(View view, String message, int duration) {
+        Snackbar.make(view, message, duration).show();
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            Intent intent = new Intent(Login.this, Login.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("EXIT", true);
+            startActivity(intent);
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Tekan Lagi Untuk Keluar", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
--- app/src/main/java/com/example/asus/shamind/Register.java
+++ app/src/main/java/com/example/asus/shamind/Register.java
@@ -1,28 +1,125 @@
 package com.example.asus.shamind;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class Register extends AppCompatActivity {
 
-    private TextView Login;
+    //defining view objects
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editReTextPassword;
+    private Button Register;
+    private ProgressDialog progressDialog;
+
+    //defining firebaseauth object
+    private String userID;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Login = (TextView) findViewById(R.id.Btn_login);
 
-        Login.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //initializing views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmailRegister);
+        editTextPassword = (EditText) findViewById(R.id.editTextPasswordRegister);
+        editReTextPassword = (EditText) findViewById(R.id.editTextRePasswordRegister);
+        Register = (Button) findViewById(R.id.button_register);
+
+        progressDialog = new ProgressDialog(this);
+
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mIntent = new Intent(Register.this, Login.class);
-                startActivity(mIntent);
+                //getting email and password from edit texts
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                String repassword = editReTextPassword.getText().toString().trim();
+
+                //checking if email and passwords are empty
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Register.this, "Please enter email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password) && TextUtils.isEmpty(repassword)) {
+                    Toast.makeText(Register.this, "Please enter password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (password.equals(repassword)) {
+                } else {
+                    Toast.makeText(Register.this, "Password Tidak Sesuai", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //if the email and password are not empty
+                //displaying a progress dialog
+
+                progressDialog.setMessage("Tunggu Sebentar...");
+                progressDialog.show();
+
+                //creating a new user
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                //checking if success
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    userID = user.getUid();
+                                    //display some message here
+                                    Intent mIntent = new Intent(Register.this, Home.class);
+                                    startActivity(mIntent);
+                                    Toast.makeText(Register.this, "Successfully registered ", Toast.LENGTH_LONG).show();
+                                } else {
+                                    //display some message here
+                                    Toast.makeText(Register.this, "Registration Error", Toast.LENGTH_LONG).show();
+                                }
+                                progressDialog.dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Register.this, "Registration Error", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
+
     }
 }
